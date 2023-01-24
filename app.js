10cc7d74cf6e2fdfaadd809f6d8799a27cae2899
Huynh <hhuynh@una.edu>
2023-01-24 16:52:34 -0600

    set up ejs engine

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) {
-  res.send('<!DOCTYPE html><html lang="en"> <h1>Hello World, is it November, date: Jan 24, 2023!</h1>')
+//set to the view engine to ejs
+app.set('view engine', 'ejs');
+
+
+let myName = 'HauHuynh';
+
+app.get('/', (req, res) => {
+  //res.send('<!DOCTYPE html><html lang="en"> <h1>Hello World, is it November, date: Jan 24, 2023!</h1>')
+
+    res.send('<h3> Hi, ${ myName} </h3>')
+
+
 })
 
 
@@ -12,6 +22,16 @@ app.get('/show',(req, res) => {
     
 
     res.sendFile('index.html') ,{root : __dirname};
+
+})
+
+app.get('/ejs', (req,res) => {
+
+    // ejs stuff goes here
+
+    // use res.render to load up an ejs view file
+    res.render('index');
+
 
 })
 
