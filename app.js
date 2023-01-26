2a560548d5de8c32efef871456ede2fbc9ff0f52
Huynh <hhuynh@una.edu>
2023-01-26 16:46:30 -0600

    Thursdat got client to node data passed using form

--- app.js
+++ app.js
@@ -2,19 +2,22 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000;  
 
-//set the view engine to ejs
+// set the view engine to ejs
 
 app.set('view engine', 'ejs');
 
 
-let myName = 'Hau Huynh'; 
+let myName = 'Hau Anh Huynh'; 
 
 app.get('/', async (req, res) => {
 //res.send('<!DOCTYPE html><html lang="en"><h1>Hello World, is it November?</h1>')
 
 //res.send(myName);
-  let result = await res.send(`<h3> Hi, ${ myName } </h3>`)
-  console/log (myName);
+
+  let result = await res.send(`<h3> Hi, ${ myName } </h3>`);
+  
+  console.log( myName); 
+  
 
 })
 
@@ -24,27 +27,29 @@ app.get('/show', (req, res) => {
     res.sendFile('index.html' , { root : __dirname});
 })
 
+console.log("before app dot get to slash ejs", myName);
 
 app.get('/ejs', (req,res) => {
-    console.log("in/ejs render/ejs", myName);
-    //ejs stuff goes here.     
+
+    //ejs stuff goes here. 
+
+    console.log("in /ejs before render:", myName);
 
     // use res.render to load up an ejs view file
-    res.render('index', { myName: myName } ); // left one is ejs, right side is node land
-    console.log("after res render/ejs", myName);
-
+    res.render('index', { myName: myName } ); //left one is ejs, right side is node land
+    console.log("after res render /ejs:", myName);
 })
 
+app.get('/name', (req,res) => {
 
-app.get('/name', (req, res) => {
-
-  console.log("in get slash name:", req.query.ejsFormName);
-
+  console.log("in get to slash name:", req.query.ejsFormName); 
+  
 })
 
-
+console.log("after app dot get to slash ejs", myName);
 
 console.log('in the node console');
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${ PORT }`)
 })
